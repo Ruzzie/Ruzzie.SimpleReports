7383b67328a055d3d0111e3a8ae12fc4486763fa
Ruzzie <dorus.verhoeckx@gmail.com>
2021-11-07 15:23:55 +0100

    [added] was still used in consuming code

---
+++ source/SimpleReports/Ruzzie.SimpleReports/RunParamsHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Ruzzie.Common.Types;
+
+namespace Ruzzie.SimpleReports
+{
+    public static class RunParamsHelper
+    {
+        public static Option<string> GetParameterFromRunParams(ReadOnlySpan<(string Name, string Value)> runParams, string parameterName)
+        {
+            //Todo: check if we can optimize the runParams data structure; maybe a Dictionary...?
+            var result = Option.None<string>();
+
+            for (var i = 0; i < runParams.Length; i++)
+            {
+                (string name, string value) = runParams[i];
+
+                if (name.Equals(parameterName, StringComparison.OrdinalIgnoreCase))
+                {
+                    result = value;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
